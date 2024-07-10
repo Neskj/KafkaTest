f3ef4abb49c72b926e243962cc07ff820eef28f3
Neskj <pronin.qq@gmail.com>
2024-07-10 12:59:15 +0300

    add Kafka producer & consumer projects files

---
+++ KafkaProducer/KafkaProducer/src/main/java/neskj/KafkaProducer/KafkaProducerApplication.java
@@ -0,0 +1,15 @@
+package neskj.KafkaProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(KafkaProducerApplication.class, args);
+		System.out.println("Hello from Kafka Producer !!!");
+	}
+
+}
